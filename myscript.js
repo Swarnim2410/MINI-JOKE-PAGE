a768dd1954a76e5256cc24c3bd7dcb18035d6442
Swarnim2410 <swarnimagupta2124@gmail.com>
2023-09-19 00:43:57 +0530

    initial commit

---
+++ myscript.js
@@ -0,0 +1,40 @@
+// https://icanhazdadjoke.com/
+const jokes = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+//using promises -->
+// const generateJokes = () => {
+//     const setHeader = {
+//         headers:
+//         {
+//             Accept: "application/json"
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', setHeader)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokes.innerHTML = data.joke;
+//         }).catch((error) => {
+//             console.log(error);
+//         })
+// }
+
+//using async js -->
+const generateJokes = async () => {
+    try {
+        const setHeader = {
+            headers:
+            {
+                Accept: "application/json"
+            }
+        }
+        const res = await fetch("https://icanhazdadjoke.com/", setHeader);
+        const data = await res.json();
+        jokes.innerHTML = data.joke;
+    } catch (err) {
+        console.log(`The error is ${err}`);
+    }
+}
+
+jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
